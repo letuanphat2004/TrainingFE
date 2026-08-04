bbca7b591a26136fa5503f62eb39838f437f58e6
Le Tuan Phat <phatletuan153@gmail.com>
2026-08-04 11:03:53 +0700

    refactor(home-1): separate header and slider blocks

--- apps/01-react-css/src/pages/HomeOne/HomeOnePage.tsx
+++ apps/01-react-css/src/pages/HomeOne/HomeOnePage.tsx
@@ -1,8 +1,9 @@
-import { HomeHero } from "../../components/home-one/HomeHero";
+import { MainSlider } from "../../components/home-one/MainSlider";
 import { CoreServices } from "../../components/home-one/CoreServices";
 import { AboutUs } from "../../components/home-one/AboutUs";
 import { ProfessionalTeams } from "../../components/home-one/ProfessionalTeams";
 import { ContactUs } from "../../components/home-one/ContactUs";
+import { Header } from "../../components/layout/Header";
 import { Footer } from "../../components/layout/Footer";
 import { assets } from "../../constants/assets";
 import { homeOneFrames } from "./homeOne.frames";
@@ -13,20 +14,20 @@ export function HomeOnePage() {
         "--page-reference-width": `${homeOneFrames.page.width}px`,
         "--page-reference-height": `${homeOneFrames.page.height}px`,
     };
-    const footerStageStyle: CssVariableStyle = {
-        "--footer-stage-background": `url("${assets.footer.background}")`,
-    };
     return (<main className="home-one-page" data-page="home-one" style={pageStyle}>
-      <HomeHero />
-      <div className="home-one-page__content-stage">
-        <img className="home-one-page__services-about-background" src={assets.homeOne.servicesAboutBackground} alt="" aria-hidden="true" />
+      <div className="home-one-page__background-layer" aria-hidden="true">
+        <img className="home-one-page__background home-one-page__background--bubble-1" src={assets.homeOne.slideBackground} alt="" />
+        <img className="home-one-page__background home-one-page__background--bubble-3" src={assets.homeOne.servicesAboutBackground} alt="" />
+        <img className="home-one-page__background home-one-page__background--bubble-2" src={assets.homeOne.contactBackground} alt="" />
+        <img className="home-one-page__background home-one-page__background--footer" src={assets.footer.background} alt="" />
+      </div>
+      <div className="home-one-page__content">
+        <Header activePage="home" />
+        <MainSlider />
         <CoreServices />
         <AboutUs />
         <ProfessionalTeams />
-        <img className="home-one-page__team-contact-background" src={assets.homeOne.contactBackground} alt="" aria-hidden="true" />
         <ContactUs />
-      </div>
-      <div className="home-one-page__footer-stage" style={footerStageStyle}>
         <Footer />
       </div>
     </main>);
